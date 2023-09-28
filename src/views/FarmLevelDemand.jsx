51bfe6d0f56e38b782a482ca7c51ef10113a7618
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-09-28 17:03:59 +0530

    added params

---
+++ src/views/FarmLevelDemand.jsx
@@ -0,0 +1,31 @@
+import FormWithTabs from "../components/generic/FormWithTabs";
+import database from "../data/db.json";
+
+const FarmLevelDemand = (props) => {
+  const items = [
+    {
+      key: "1",
+      label: "PROFILE",
+      data: database.farmer_profile_form,
+    },
+    {
+      key: "2",
+      label: "LAND",
+
+      data: database.land_details_form,
+    },
+    {
+      key: "3",
+      label: "LIVESTOCK",
+      data: database.livestock_form,
+    },
+  ];
+
+  return (
+    <>
+      <FormWithTabs data={items} tele={props.tele} />
+    </>
+  );
+};
+
+export default FarmLevelDemand;
